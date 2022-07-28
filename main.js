fdbac5be33a42dab5afa1045022e4a2ce9f17c8d
Everette Esguerra <emayesguer15ra@gmail.com>
2022-07-28 16:55:01 -0700

    Add playRound function

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
+//call getComputerChoice()
+getComputerChoice();
+
+
+
+
+
+
+
+
+
 //create function called getComputerChoice
 function getComputerChoice(){
     //create variable randomNumber
     let randomNumber;
+   
     //generate a random number 1 - 3 
     //and store in randomNumber
     randomNumber = Math.floor(Math.random()*(3) + 1);
+    
     //create variable computerSelection
     let computerSelection;
+    
     //create switch statement with condition randomNumber
     switch (randomNumber){
         //if number is 1, then return "ROCK"
@@ -25,12 +39,32 @@ function getComputerChoice(){
 }
 //create function playRound with inputs
 //playerSelection and computerSelection
-//make player selection uppercase
-    //if playerSelection is equal to computerSelection
-        //output 'oops its a tie'
+function playRound(playerSelection, computerSelection){
+    //make playerSelection uppercase and store in upperPlayerSelection
+    let upperPlayerSelection = playerSelection.toUpperCase();
+    
+    //create variables for win, lose, and tie messages
+    let winMessage = `You win! ${upperPlayerSelection} beats ${computerSelection}!`;
+    let loseMessage = `You lose! ${computerSelection} beats ${upperPlayerSelection}!`;
+    let tieMessage = "Oops! It's a tie! No one wins!";
+    
+    //if upperPlayerSelection is equal to computerSelection
+    if (upperPlayerSelection === computerSelection){
+        //output tieMessage
+        return tieMessage;
+
     //if there is a win condition for player
-        //output winMessage
+    } else if ((upperPlayerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+               (upperPlayerSelection === 'PAPER' && computerSelection === 'ROCK') ||
+               (upperPlayerSelection === 'SCISSORS' && computerSelection === 'PAPER')
+    ){  //output winMessage
+        return winMessage;
+
     //if there is a lose condition for player
-        //output loseMessage
-
-
+    } else if ((upperPlayerSelection === 'ROCK' && computerSelection === 'PAPER') ||
+               (upperPlayerSelection === 'PAPER' && computerSelection === 'SCISSORS') ||
+               (upperPlayerSelection === 'SCISSORS' && computerSelection === 'ROCK')
+    ){  //output loseMessage
+        return loseMessage;
+    }
+}
