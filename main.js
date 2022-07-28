19158236290a8c72f99862c6774fe51e326b6f04
Everette Esguerra <emayesguer15ra@gmail.com>
2022-07-28 15:13:15 -0700

    Add pseudocode to main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+//create function called getComputerChoice
+//generate random number 1 - 3
+//if number is 1, then return "ROCK"
+//if number is 2, then return "PAPER"
+//if number is 3, then return "SCISSORS"
