2d00552529f88f007c8182a7144692de082b1229
Everette Esguerra <emayesguer15ra@gmail.com>
2022-07-28 15:55:10 -0700

    Add getComputerChoice() function

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 //create function called getComputerChoice
-//generate random number 1 - 3
-//if number is 1, then return "ROCK"
-//if number is 2, then return "PAPER"
-//if number is 3, then return "SCISSORS"
+function getComputerChoice(){
+    //create variable randomNumber
+    let randomNumber;
+    //generate a random number 1 - 3 
+    //and store in randomNumber
+    randomNumber = Math.floor(Math.random()*(3) + 1);
+    //create variable computerSelection
+    let computerSelection;
+    //create switch statement with condition randomNumber
+    switch (randomNumber){
+        //if number is 1, then return "ROCK"
+        case 1:
+            computerSelection = 'ROCK';
+            break;
+        //if number is 2, then return "PAPER"
+        case 2:
+            computerSelection = 'PAPER';
+            break;
+        //if number is 3, then return "SCISSORS"
+        case 3:
+            computerSelection = 'SCISSORS'
+    }
+    return computerSelection;
+}
+getComputerChoice();
+
+
